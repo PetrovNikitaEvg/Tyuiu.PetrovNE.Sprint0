7feaa41bb5a4e4320c52a6fc9353b4a3f8e5ec72
PetrovNikitaEvg <Shug1zawa@yandex.ru>
2023-09-16 23:43:24 +0500

    Task2

---
+++ Tyuiu.PetrovNE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PetrovNE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //Область создания методов тестирования методов из библиотеки
+            var name = "Никита";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Никита", res);
+        }
+    }
+}
