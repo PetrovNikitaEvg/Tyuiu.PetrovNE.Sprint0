1c2b20b8b35e206eabd9038e6e47b8f2b31f7c80
PetrovNikitaEvg <Shug1zawa@yandex.ru>
2023-09-16 20:54:50 +0500

    Task1

---
+++ Tyuiu.PetrovNE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PetrovNE.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            String Name = "Никита";
+            int Age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + Name + " мне " + Age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
